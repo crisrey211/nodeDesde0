caf56f9eff144a34dbae409d1b99b52837c4b1db
christian_reynaltt <creynaltt@maristak.net>
2023-09-27 23:50:48 +0200

    video 7 sin terminar --> Router y midleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 console.clear;
 import express from 'express';
+import dotenv from "dotenv"
+import { USER_BBDD} from './bbdd.js'
 
+dotenv.config()
 const PORT  = 3000;
 const expressApp = express()
 
@@ -9,12 +12,47 @@ expressApp.use(express.json())
 expressApp.use(express.text())
 
 
-expressApp.post('/cuenta', (req, res) => {
-    console.log(req.query)
-    /* res.status(400).send({
-        errroMessage: "NO autorizado"
-    }) */
-    res.send("tu cuenta personal")
+//Obtener los detalles de la cuenta a partir de la cuenta a partir del guid
+expressApp.get('/account/:guid', (req, res) => { 
+    const { guid } = req.params
+    
+    const user = USER_BBDD.find(user => user.guid === guid)
+    console.log(guid,user);
+    if(!user) res.status(400).send("erro al obtener resultados")
+    return res.send(user)
+})
+
+//Crear una cuenta
+expressApp.post('/account', (req, res) => {
+    const { guid, name } = req.body
+    if (!guid || !name) res.status(400).send("No se ha podido crear cuenta")
+    const user = USER_BBDD.find(user => user.guid === guid)
+    if (user) res.status(409).send("La cuenta ya existe")
+
+    USER_BBDD.push({
+        guid, name
+    })
+    return res.send("se hareadfo bien")
+})
+
+//Actualizar una cuenta
+expressApp.patch('/account/:guid', (req, res) => {
+    const { guid , name} = req.body
+    const user = USER_BBDD.find(user => user.guid === guid)
+    if (!user) res.status(400).send("No se ha podido actualizar cuenta")
+    user.name = name
+    return res.send(user)
+})
+
+//Eliminar una cuenta
+expressApp.delete('/account/:guid', (req, res) => {
+    const { guid } = req.params
+    const userIndex = USER_BBDD.findIndex(user => user.guid === guid)
+
+    if (userIndex === -1) res.status(400).send("No se ha podido eliminar la cuenta")
+    
+    USER_BBDD.splice(userIndex, 1)
+    return res.send()
 })
 
 expressApp.listen(PORT, () => { 
---
+++ bbdd.js
@@ -0,0 +1,272 @@
+export const USER_BBDD = [
+  {
+    "_id": "6510b6f7696425789482cf93",
+    "index": 0,
+    "guid": "28caa109-f812-4e96-98b6-493894cb4c3f",
+    "isActive": true,
+    "balance": "$1,568.26",
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "eyeColor": "brown",
+    "name": "Clark Stephenson",
+    "gender": "male",
+    "company": "COMTRACT",
+    "email": "clarkstephenson@comtract.com",
+    "phone": "+1 (865) 482-2033",
+    "address": "932 Ryder Avenue, Wright, Oklahoma, 1503",
+    "about": "Reprehenderit ut reprehenderit ad occaecat eiusmod duis do laboris velit eu est. Ullamco ullamco qui consequat cillum consectetur officia aliqua consequat incididunt reprehenderit ad exercitation. Velit in aliqua labore eu qui in ad eu dolor tempor consectetur tempor eu anim. Culpa anim veniam ut aliquip voluptate Lorem. Ipsum enim non excepteur tempor adipisicing enim.\r\n",
+    "registered": "2014-08-09T06:57:06 -02:00",
+    "latitude": 4.736305,
+    "longitude": 12.289753,
+    "tags": [
+      "non",
+      "proident",
+      "do",
+      "do",
+      "laboris",
+      "ea",
+      "minim"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Katheryn Daniels"
+      },
+      {
+        "id": 1,
+        "name": "Tucker Edwards"
+      },
+      {
+        "id": 2,
+        "name": "Antoinette Rosales"
+      }
+    ],
+    "greeting": "Hello, Clark Stephenson! You have 2 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "6510b6f7a3d6eabb046644ff",
+    "index": 1,
+    "guid": "b22f13b7-f21e-4be8-84e9-a5e127e02e8b",
+    "isActive": false,
+    "balance": "$3,243.04",
+    "picture": "http://placehold.it/32x32",
+    "age": 29,
+    "eyeColor": "blue",
+    "name": "Jannie Herman",
+    "gender": "female",
+    "company": "INSURETY",
+    "email": "jannieherman@insurety.com",
+    "phone": "+1 (950) 420-2856",
+    "address": "367 Pineapple Street, Fairlee, Delaware, 4702",
+    "about": "Ut aute pariatur culpa fugiat. Veniam consectetur cillum excepteur et nisi fugiat cupidatat laborum excepteur ex labore aute. Dolor eu veniam adipisicing quis laborum quis do magna sint tempor veniam qui.\r\n",
+    "registered": "2017-04-13T06:35:15 -02:00",
+    "latitude": 3.095915,
+    "longitude": 54.969726,
+    "tags": [
+      "consectetur",
+      "qui",
+      "dolor",
+      "incididunt",
+      "adipisicing",
+      "ea",
+      "laborum"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Moon Collier"
+      },
+      {
+        "id": 1,
+        "name": "Bernard Wright"
+      },
+      {
+        "id": 2,
+        "name": "Leta Hodges"
+      }
+    ],
+    "greeting": "Hello, Jannie Herman! You have 7 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "6510b6f7a2781c981dc469bd",
+    "index": 2,
+    "guid": "de046ef5-e11f-4bf9-b47c-835f5fe272d4",
+    "isActive": true,
+    "balance": "$1,132.90",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "eyeColor": "blue",
+    "name": "Caitlin Meyer",
+    "gender": "female",
+    "company": "ZOLARITY",
+    "email": "caitlinmeyer@zolarity.com",
+    "phone": "+1 (962) 519-3055",
+    "address": "669 Lawton Street, Trucksville, Northern Mariana Islands, 9469",
+    "about": "Irure quis reprehenderit labore ipsum. Velit laboris in et amet Lorem. Excepteur eu ex mollit adipisicing exercitation est. Dolor sunt ea aliqua aute duis eu tempor laboris veniam dolore. Sint dolor dolor est velit minim enim veniam adipisicing in qui exercitation cillum nisi. Ad eiusmod mollit aute exercitation esse do. Est sunt reprehenderit officia cupidatat labore irure aliqua qui deserunt quis mollit amet enim ex.\r\n",
+    "registered": "2016-03-17T03:07:44 -01:00",
+    "latitude": 85.242721,
+    "longitude": 17.120746,
+    "tags": [
+      "consequat",
+      "laborum",
+      "mollit",
+      "adipisicing",
+      "labore",
+      "in",
+      "do"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Nadia Castro"
+      },
+      {
+        "id": 1,
+        "name": "Nixon Leonard"
+      },
+      {
+        "id": 2,
+        "name": "Waters Woods"
+      }
+    ],
+    "greeting": "Hello, Caitlin Meyer! You have 2 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "6510b6f7246877ffc9be8e80",
+    "index": 3,
+    "guid": "b42c686a-e76a-4941-8e14-726db5ade86c",
+    "isActive": false,
+    "balance": "$3,621.29",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "brown",
+    "name": "Lindsey Merrill",
+    "gender": "male",
+    "company": "VERAQ",
+    "email": "lindseymerrill@veraq.com",
+    "phone": "+1 (998) 414-3708",
+    "address": "578 Vernon Avenue, Fostoria, Vermont, 1120",
+    "about": "Labore esse tempor sit ex excepteur anim irure cupidatat veniam aliquip. Minim nostrud quis consequat aliqua eiusmod fugiat quis ipsum ut in. Reprehenderit adipisicing minim veniam eu fugiat in laboris adipisicing dolore dolor esse aliquip. Esse cupidatat non tempor anim quis reprehenderit voluptate ea. Non nisi enim sit enim occaecat. Cillum reprehenderit ut cillum et aliquip nulla voluptate irure.\r\n",
+    "registered": "2017-08-18T07:59:31 -02:00",
+    "latitude": 7.018683,
+    "longitude": 143.948072,
+    "tags": [
+      "laboris",
+      "velit",
+      "anim",
+      "reprehenderit",
+      "ex",
+      "do",
+      "fugiat"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Elma Rodriquez"
+      },
+      {
+        "id": 1,
+        "name": "Juanita Parker"
+      },
+      {
+        "id": 2,
+        "name": "Angelica Klein"
+      }
+    ],
+    "greeting": "Hello, Lindsey Merrill! You have 1 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "6510b6f74e76f76399e46e87",
+    "index": 4,
+    "guid": "ac23178a-1079-4e92-9b91-dd09ef2cc514",
+    "isActive": false,
+    "balance": "$3,514.04",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "brown",
+    "name": "Golden Marks",
+    "gender": "male",
+    "company": "ISOTERNIA",
+    "email": "goldenmarks@isoternia.com",
+    "phone": "+1 (963) 567-2908",
+    "address": "607 Lefferts Place, Thynedale, Missouri, 9504",
+    "about": "Exercitation cillum laboris exercitation consequat velit adipisicing reprehenderit velit occaecat pariatur. Consequat ipsum eiusmod cupidatat occaecat elit eu. Aliqua aliqua nulla nisi quis commodo duis eu Lorem esse consectetur irure non ad ut. In non tempor dolore velit sunt anim labore mollit duis velit commodo.\r\n",
+    "registered": "2022-09-21T06:32:37 -02:00",
+    "latitude": -54.602911,
+    "longitude": -26.962264,
+    "tags": [
+      "occaecat",
+      "veniam",
+      "esse",
+      "enim",
+      "esse",
+      "duis",
+      "nulla"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Mathews Moody"
+      },
+      {
+        "id": 1,
+        "name": "Robyn Bryan"
+      },
+      {
+        "id": 2,
+        "name": "Therese Holland"
+      }
+    ],
+    "greeting": "Hello, Golden Marks! You have 8 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "6510b6f7d5f880ca45b7a72d",
+    "index": 5,
+    "guid": "56acde73-13cf-4beb-ac32-6941503f64e0",
+    "isActive": true,
+    "balance": "$3,644.59",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "eyeColor": "blue",
+    "name": "Kaufman Hyde",
+    "gender": "male",
+    "company": "INQUALA",
+    "email": "kaufmanhyde@inquala.com",
+    "phone": "+1 (911) 404-2924",
+    "address": "610 Ferris Street, Cloverdale, Massachusetts, 3166",
+    "about": "Ad laboris velit ut quis. Do ut magna consectetur ut exercitation voluptate aliqua elit. Consequat sint ad voluptate officia reprehenderit commodo fugiat irure magna officia esse nisi velit. Et velit nulla aliqua quis sit sit elit ullamco laborum non adipisicing nostrud.\r\n",
+    "registered": "2022-12-03T12:31:12 -01:00",
+    "latitude": 38.971701,
+    "longitude": 81.110683,
+    "tags": [
+      "occaecat",
+      "laboris",
+      "in",
+      "elit",
+      "sit",
+      "non",
+      "magna"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Doyle Franklin"
+      },
+      {
+        "id": 1,
+        "name": "Hallie Holloway"
+      },
+      {
+        "id": 2,
+        "name": "Joan Rice"
+      }
+    ],
+    "greeting": "Hello, Kaufman Hyde! You have 7 unread messages.",
+    "favoriteFruit": "strawberry"
+  }
+]
