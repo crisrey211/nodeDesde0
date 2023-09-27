c056d9af1d4441b3de53104ea96751b14b546d9e
christian_reynaltt <creynaltt@maristak.net>
2023-09-28 00:04:39 +0200

    antes de ir a dormir

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.clear;
 import express from 'express';
 import dotenv from "dotenv"
-import { USER_BBDD} from './bbdd.js'
+import accountRouter from './routes/account.js'
 
 dotenv.config()
 const PORT  = 3000;
@@ -10,50 +10,10 @@ const expressApp = express()
 //Middelware sirven para recibir el body
 expressApp.use(express.json())
 expressApp.use(express.text())
+expressApp.use(accountRouter)
 
 
-//Obtener los detalles de la cuenta a partir de la cuenta a partir del guid
-expressApp.get('/account/:guid', (req, res) => { 
-    const { guid } = req.params
-    
-    const user = USER_BBDD.find(user => user.guid === guid)
-    console.log(guid,user);
-    if(!user) res.status(400).send("erro al obtener resultados")
-    return res.send(user)
-})
 
-//Crear una cuenta
-expressApp.post('/account', (req, res) => {
-    const { guid, name } = req.body
-    if (!guid || !name) res.status(400).send("No se ha podido crear cuenta")
-    const user = USER_BBDD.find(user => user.guid === guid)
-    if (user) res.status(409).send("La cuenta ya existe")
-
-    USER_BBDD.push({
-        guid, name
-    })
-    return res.send("se hareadfo bien")
-})
-
-//Actualizar una cuenta
-expressApp.patch('/account/:guid', (req, res) => {
-    const { guid , name} = req.body
-    const user = USER_BBDD.find(user => user.guid === guid)
-    if (!user) res.status(400).send("No se ha podido actualizar cuenta")
-    user.name = name
-    return res.send(user)
-})
-
-//Eliminar una cuenta
-expressApp.delete('/account/:guid', (req, res) => {
-    const { guid } = req.params
-    const userIndex = USER_BBDD.findIndex(user => user.guid === guid)
-
-    if (userIndex === -1) res.status(400).send("No se ha podido eliminar la cuenta")
-    
-    USER_BBDD.splice(userIndex, 1)
-    return res.send()
-})
 
 expressApp.listen(PORT, () => { 
     console.log(`Escuchando en el puerto ${PORT} `)
--- routes/account.js
+++ routes/account.js
@@ -1,3 +1,50 @@
 import express from "express";
+import { USER_BBDD } from '../bbdd.js'
 
 const accountRouter = express.Router()
+
+
+//Obtener los detalles de la cuenta a partir de la cuenta a partir del guid
+accountRouter.get('/account/:guid', (req, res) => { 
+    const { guid } = req.params
+    
+    const user = USER_BBDD.find(user => user.guid === guid)
+    console.log(guid,user);
+    if(!user) res.status(400).send("erro al obtener resultados")
+    return res.send(user)
+})
+
+//Crear una cuenta
+accountRouter.post('/account', (req, res) => {
+    const { guid, name } = req.body
+    if (!guid || !name) res.status(400).send("No se ha podido crear cuenta")
+    const user = USER_BBDD.find(user => user.guid === guid)
+    if (user) res.status(409).send("La cuenta ya existe")
+
+    USER_BBDD.push({
+        guid, name
+    })
+    return res.send("se hareadfo bien")
+})
+
+//Actualizar una cuenta
+accountRouter.patch('/account/:guid', (req, res) => {
+    const { guid , name} = req.body
+    const user = USER_BBDD.find(user => user.guid === guid)
+    if (!user) res.status(400).send("No se ha podido actualizar cuenta")
+    user.name = name
+    return res.send(user)
+})
+
+//Eliminar una cuenta
+accountRouter.delete('/account/:guid', (req, res) => {
+    const { guid } = req.params
+    const userIndex = USER_BBDD.findIndex(user => user.guid === guid)
+
+    if (userIndex === -1) res.status(400).send("No se ha podido eliminar la cuenta")
+    
+    USER_BBDD.splice(userIndex, 1)
+    return res.send()
+})
+
+export default accountRouter
