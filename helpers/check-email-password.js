00c089b6513855e8505009b6d1f44344a5df4924
christian_reynaltt <creynaltt@maristak.net>
2023-09-29 01:00:04 +0200

    autorizacion y autenticaión funcionando

--- helpers/check-email-password.js
+++ helpers/check-email-password.js
@@ -1,5 +1,12 @@
-const checkEmailPassword = () => {
-    
-}
+import { USER_BBDD } from '../bbdd.js';
 
-export default checkEmailPassword
+const authByEmailPwd = (email, password) => {
+  const user = USER_BBDD.find((user) => user.email === email);
+  if (!user) throw new Error();
+
+  if (user.password !== password) return res.send(401).send('No autenticado');
+
+  return user;
+};
+
+export default authByEmailPwd;
--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,38 @@
-import { Router } from "express";
-import { USER_BBDD } from "../bbdd";
+import { Router } from 'express';
+import { USER_BBDD } from '../bbdd.js';
+import authByEmailPwd from '../helpers/check-email-password.js';
 
-const authRouter = Router()
+const authRouter = Router();
 
-//Endpoint público (No autenticado y no autorizado) 
-authRouter.get("publico", (req, res) => res.send("endpoint publico") )
+//Endpoint público (No autenticado y no autorizado)
+authRouter.get('/publico', (req, res) => res.send('endpoint publico'));
 
 //Endpoint autenticado para todo usuario registrado
-authRouter.post("autenticado", (req, res) => {
-    const { email, password } = req.body
-    if (!email || !password) return res.status(400).send("mo te has autenticado")
+authRouter.post('/autenticado', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) return res.status(400).send('mo te has autenticado');
 
-    const user = USER_BBDD.filter((user) => user.email === email)
-    if (!user) return res.send(401).send("No autenticado")
-    
-    if(user.password === password ) return res.send(401).send("No autenticado") 
-    
-    res.send(`Usuario ${user.name} autenticado`)
-})
+  try {
+    const user = authByEmailPwd(email, password);
+    res.send(`Usuario ${user.name} autenticado`);
+  } catch (erro) {
+    return res.send(401);
+  }
+});
 
 //Endpoint autorizado a administradores
-authRouter.post("autorizado", (req, res) => {
-    const { email, password } = req.body
-    if (!email || !password) return res.status(400).send("mo te has autenticado")
+authRouter.post('/autorizado', (req, res) => {
+  const { email, password } = req.body;
 
-    const user = USER_BBDD.filter((user) => user.email === email)
-    if (!user) return res.send(401).send("No autenticado")
-    
-    if(user.password === password ) return res.send(401).send("No autenticado") 
-    
-    if(user.role==='admin') return res.send(403).send("No autorizado") 
+  if (!email || !password) return res.status(400).send('No te has autenticado');
 
-    res.send(`Usuario adminstrado ${user.name}`)
-})
+  try {
+    const user = authByEmailPwd(email, password);
+    if (user.role !== 'admin') return res.send(403).send('No autorizado');
+    res.send(`Usuario adminstrado ${user.name}`);
+  } catch (erro) {
+    return res.sendStatus(401).send('Erro en la autoriazación');
+  }
+});
 
-
-export default authRouter
+export default authRouter;
