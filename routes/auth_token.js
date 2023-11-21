ba8f3da742360bd81abf3dab12a106a523b59817
christian_reynaltt <creynaltt@maristak.net>
2023-11-22 00:01:57 +0100

    Autentiacion por Token terminado

--- routes/auth_token.js
+++ routes/auth_token.js
@@ -1,38 +1,58 @@
 import { Router } from 'express';
 import authByEmailPwd from '../helpers/check-email-password.js';
-import { SignJWT } from 'jose';
+import { SignJWT, jwtVerify } from 'jose';
 import { USER_BBDD } from '../bbdd.js';
 
 const authTokenRouter = Router();
 
 //Login con email y password
 
-authSessionRouter.post('/login', (req, res) => {
+authTokenRouter.post('/login', async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) return res.status(400);
 
   try {
-    const user = authByEmailPwd(email, password);
+    const { guid } = authByEmailPwd(email, password);
     //GENERAR TOKEN Y DEVOLVER TOKEN
+    const jwtConstructor = new SignJWT({ guid });
 
-    return res.send(`Usuario ${user.name} autenticado`);
+    const enconder = new TextEncoder();
+    const jwt = await jwtConstructor
+      .setProtectedHeader({ alg: 'HS256', typ: 'jwt' })
+      .setIssuedAt()
+      .setExpirationTime('1h')
+      .sign(enconder.encode(process.env.JWT_PRIVATE_KEY));
+
+    return res.send({ jwt });
   } catch (error) {
     return res.sendStatus(401);
   }
 });
 
 //Solicitud autenticada con token para obtener el perfil del usuario
-authSessionRouter.get('/profile', (req, res) => {
-  //OBTENER TOKEN DE CABECERA Y COMPORBAR SU AUTENTICIDAD Y CADUCIDAD
+authTokenRouter.get('/profile', async (req, res) => {
+  const { authorization } = req.headers;
 
-  const user = USER_BBDD.find((user) => user.guid === userSession.guid);
+  if (!authorization) return res.sendStatus(401);
+  try {
+    const enconder = new TextEncoder();
+    const { payload } = await jwtVerify(
+      authorization,
+      enconder.encode(process.env.JWT_PRIVATE_KEY),
+    ); //el segundo parámetro es la clave privada
 
-  if (!user) return res.status(401);
+    //OBTENER TOKEN DE CABECERA Y COMPORBAR SU AUTENTICIDAD Y CADUCIDAD
+    const user = USER_BBDD.find((user) => user.guid === payload.guid);
 
-  //Borramos la contraseña para no mandarla
-  delete user.password;
+    if (!user) return res.status(401);
 
-  //Mandamos el objeto user de vuelta
-  return res.send(user);
+    //Borramos la contraseña para no mandarla
+    delete user.password;
+
+    //Mandamos el objeto user de vuelta
+    return res.send(user);
+  } catch (error) {
+    return res.send(401);
+  }
 });
 export default authTokenRouter;
